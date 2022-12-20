7bc8c287d3a5cb0ab0b972c90c2eec0c7a5e4668
Ovidiu Popa <ovidiu.popa@armory.io>
2022-12-20 13:55:48 +0200

    Initial commit

---
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping(path = "/hello")
+    String hello(@RequestParam String name){
+        return "Hello " + name + " !";
+    }
+}
